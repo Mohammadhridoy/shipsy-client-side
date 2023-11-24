2adce837891b6fbdfafcc62ae6aed3e48fcd647f
Mohammadhridoy <mdridoykha019@gmail.com>
2023-11-24 11:48:04 +0600

    create client side

---
+++ src/Firebase/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBiXJwHPxRn2RYGPp8a-dcTKdYZSNZ3T2s",
+  authDomain: "shipsy-520a6.firebaseapp.com",
+  projectId: "shipsy-520a6",
+  storageBucket: "shipsy-520a6.appspot.com",
+  messagingSenderId: "745528507327",
+  appId: "1:745528507327:web:9eec74dea5ddfe07b93f0a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
